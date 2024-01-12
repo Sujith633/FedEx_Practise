43739b14b0dcb3790c0249cd7ac9d0cc8dcdc182
Pullaiah Gari <spullaiahgari@teksystems.com>
2024-01-12 09:53:03 +0530

    CSVTOCSV

--- S02SpringBatchCSV/src/main/java/com/example/S02SpringBatchCSV/PersonItemProcessor.java
+++ S02SpringBatchCSV/src/main/java/com/example/S02SpringBatchCSV/PersonItemProcessor.java
@@ -11,39 +11,12 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 
 public class PersonItemProcessor implements ItemProcessor<Person, Person>{
 
 	 private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
 
-//	  @Override
-//	  public Person process(final Person person) {
-////		  System.out.println("Iem Processor Running");
-//	    final String firstName = person.getFirstName().toUpperCase();
-//	    final String lastName = person.getLastName().toUpperCase();
-//
-//	    final Person transformedPerson = new Person(firstName,lastName);
-//
-//	    log.info("Converting (" + person + ") into (" + transformedPerson + ")");
-//
-//	    return transformedPerson;
-//	  }
-//	 private Set<String> processedRecords = new HashSet<>();
-//
-//	    @Override
-//	    public Person process(Person item) throws Exception {
-//	        if (!processedRecords.contains(item.getFirstName())) {
-//	            processedRecords.add(item.getLastName());
-//	            System.out.println(processedRecords);
-//	            log.info("Converting (" + item + ") into (" + item + ")");
-//	            return item;
-//	        } else {
-//	            return null; // Skip already processed records
-//	        }
-//	    }
 	 private Set<String> existingRecords = new HashSet<>();
 	 
 	 	public void afterPropertiesSet() throws Exception {
